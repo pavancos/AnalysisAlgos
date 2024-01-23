58de5442c333d462936e7bb22ab96b7b6c155029
Pavan Kumar Chennupati <pavankc005@gmail.com>
2024-01-23 09:57:02 +0000

    format

--- Divide and Conquer/MinMax-DivConq.cpp
+++ Divide and Conquer/MinMax-DivConq.cpp
@@ -1,29 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
-void MinMaxDAC(vector<int>&A,int i,int j,int &max,int &min) {
-    if(i == j){
-        max=A[i];
-        min=A[i];
+void MinMaxDAC(vector<int> &A, int i, int j, int &max, int &min)
+{
+    if (i == j)
+    {
+        max = A[i];
+        min = A[i];
     }
-    if(i==j-1)
-        if (A[i] < A[j]){
-            max=A[j];
-            min=A[i];
+    if (i == j - 1)
+        if (A[i] < A[j])
+        {
+            max = A[j];
+            min = A[i];
         }
-        else{
-            max=A[i];
-            min=A[j];
+        else
+        {
+            max = A[i];
+            min = A[j];
         }
-    else {
+    else
+    {
         int mid = (i + j) / 2;
-        int LMax,LMin,RMax,RMin;
-        MinMaxDAC(A, i, mid,LMax,LMin);
-        MinMaxDAC(A, mid + 1, j,RMax,RMin);
-        if(LMax > RMax)
+        int LMax, LMin, RMax, RMin;
+        MinMaxDAC(A, i, mid, LMax, LMin);
+        MinMaxDAC(A, mid + 1, j, RMax, RMin);
+        if (LMax > RMax)
             max = LMax;
         else
             max = RMax;
-        if(LMin < RMin)
+        if (LMin < RMin)
             min = LMin;
         else
             min = RMin;
@@ -41,6 +46,6 @@ int main()
         arr.push_back(tmp);
     }
     int max = arr[0], min = arr[0];
-    MinMaxDAC(arr, 0, n-1,max,min);
+    MinMaxDAC(arr, 0, n - 1, max, min);
     cout << "Max: " << max << " Min: " << min << endl;
 }
