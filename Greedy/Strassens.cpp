d0cd343a94623a45541e81d39ca347c46d2d96a7
Pavan Kumar Chennupati <pavankc005@gmail.com>
2024-03-11 18:10:29 +0000

    add:strass

--- Greedy/Strassens.cpp
+++ Greedy/Strassens.cpp
@@ -1,50 +1,67 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 
-void strassenR(vector<vector<int>>& A, vector<vector<int>>& B, vector<vector<int>>& C) {
-    int m1 = (A[0][0] * B[0][1] - A[0][1] * B[1][1]) * (A[1][0] + B[0][0]);
-    int m2 = (A[0][0] + A[0][1]) * B[1][1];
-    int m3 = (A[1][0] + A[1][1]) * B[0][0];
-    int m4 = (A[1][1] * (B[1][0] - B[0][0]));
-    int m5 = (A[0][0] + A[1][1]) * (B[0][0] + B[1][1]);
-    int m6 = (A[0][1] - A[1][1]) * (B[1][0] + B[1][1]);
-    int m7 = (A[0][0] - A[1][0]) * (B[0][0] + B[0][1]);
-
-    C[0][0] = m5 + m4 - m2 + m6;
-    C[0][1] = m1 + m2;
-    C[1][0] = m3 + m4;
-    C[1][1] = m1 + m5 - m3 - m7;
+// Function to add two matrices
+void add(int A[2][2], int B[2][2], int C[2][2]) {
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 2; j++) {
+            C[i][j] = A[i][j] + B[i][j];
+        }
+    }
 }
 
-int main() {
-    vector<vector<int>> A(2, vector<int>(2));
-    vector<vector<int>> B(2, vector<int>(2));
-    vector<vector<int>> C(2, vector<int>(2));
-
-    cout << "Enter the elements of matrix A (2x2):" << endl;
+// Function to subtract two matrices
+void subtract(int A[2][2], int B[2][2], int C[2][2]) {
     for (int i = 0; i < 2; i++) {
         for (int j = 0; j < 2; j++) {
-            cin >> A[i][j];
+            C[i][j] = A[i][j] - B[i][j];
         }
     }
+}
 
-    cout << "Enter the elements of matrix B (2x2):" << endl;
+// Function to multiply two 2x2 matrices using Strassen's algorithm
+void strassen(int A[2][2], int B[2][2], int C[2][2]) {
+    int M1 = (A[0][0] + A[1][1]) * (B[0][0] + B[1][1]);
+    int M2 = (A[1][0] + A[1][1]) * B[0][0];
+    int M3 = A[0][0] * (B[0][1] - B[1][1]);
+    int M4 = A[1][1] * (B[1][0] - B[0][0]);
+    int M5 = (A[0][0] + A[0][1]) * B[1][1];
+    int M6 = (A[1][0] - A[0][0]) * (B[0][0] + B[0][1]);
+    int M7 = (A[0][1] - A[1][1]) * (B[1][0] + B[1][1]);
+
+    C[0][0] = M1 + M4 - M5 + M7;
+    C[0][1] = M3 + M5;
+    C[1][0] = M2 + M4;
+    C[1][1] = M1 - M2 + M3 + M6;
+}
+
+// Function to print a matrix
+void printMatrix(int A[2][2]) {
     for (int i = 0; i < 2; i++) {
         for (int j = 0; j < 2; j++) {
-            cin >> B[i][j];
-        }
-    }
-
-    strassenR(A, B, C);
-
-    cout << "Resultant matrix C = A * B:" << endl;
-    for (int i = 0; i < 2; i++) {
-        for (int j = 0; j < 2; j++) {
-            cout << C[i][j] << " ";
+            cout << A[i][j] << " ";
         }
         cout << endl;
     }
+}
+
+int main() {
+    int A[2][2] = {{1, 2}, {3, 4}};
+    int B[2][2] = {{5, 6}, {7, 8}};
+    int C[2][2];
+
+    strassen(A, B, C);
+
+    cout << "Matrix A:" << endl;
+    printMatrix(A);
+    cout << endl;
+
+    cout << "Matrix B:" << endl;
+    printMatrix(B);
+    cout << endl;
+
+    cout << "Resultant Matrix C:" << endl;
+    printMatrix(C);
 
     return 0;
 }
