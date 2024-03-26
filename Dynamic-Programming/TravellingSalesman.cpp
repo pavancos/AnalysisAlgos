4762c594887d8adb6f3c3d3f86506a68a17df05e
Pavan Kumar Chennupati <pavankc005@gmail.com>
2024-03-26 08:12:42 +0000

    tsp

--- Dynamic-Programming/TravellingSalesman.cpp
+++ Dynamic-Programming/TravellingSalesman.cpp
@@ -1,33 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define V 5
-int travllingSalesmanProblem(int graph[][V], int s)
-{
+int travllingSalesmanProblem(int graph[][V], int s){
     vector<int> vertex;
     for (int i = 0; i < V; i++)
         if (i != s)
             vertex.push_back(i);
     int min_path = INT_MAX;
-    do
-    {
+    do{
         int current_pathweight = 0;
         int k = s;
-        for (int i = 0; i < vertex.size(); i++)
-        {
+        for (int i = 0; i < vertex.size(); i++){
             current_pathweight += graph[k][vertex[i]];
             k = vertex[i];
         }
         current_pathweight += graph[k][s];
-
         min_path = min(min_path, current_pathweight);
-
     } while (next_permutation(vertex.begin(), vertex.end()));
 
     return min_path;
 }
-
-int main()
-{
+int main(){
     int graph[V][V] = {
         {0, 2, 7, 9, 6},
         {5, 0, 9, 7, 4},
