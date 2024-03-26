b4468c5b1ba1c998cdfd41377c3979aa8cde0e83
Pavan Kumar Chennupati <pavankc005@gmail.com>
2024-03-26 09:12:47 +0000

    add:dym

--- Dynamic-Programming/TravellingSalesman.cpp
+++ Dynamic-Programming/TravellingSalesman.cpp
@@ -19,13 +19,17 @@ int tsp(int g[][V],int s){
     return mp;
 }
 int main(){
-    int graph[V][V] = {
-        {0, 2, 7, 9, 6},
-        {5, 0, 9, 7, 4},
-        {3, 5, 0, 6, 9},
-        {4, 5, 1, 0, 6},
-        {7, 8, 3, 3, 0}
-    };
+    int graph[V][V];
+    // = {
+    //     {0, 2, 7, 9, 6},
+    //     {5, 0, 9, 7, 4},
+    //     {3, 5, 0, 6, 9},
+    //     {4, 5, 1, 0, 6},
+    //     {7, 8, 3, 3, 0}
+    // };
+    for(int i=0;i<V;i++)
+        for(int j=0;j<V;j++)
+            cin>>graph[i][j];
     int s=0;
     cout<<tsp(graph,s)<<"\n"<<endl;
     return 0;
