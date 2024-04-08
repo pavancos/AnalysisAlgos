d6c1900b83bf7d68afa2a329dbf4e8f898945e3b
Pavan Kumar Chennupati <pavankc005@gmail.com>
2024-04-08 09:47:40 +0000

    add:kn

--- Dynamic-Programming/0-1-Knapsack.cpp
+++ Dynamic-Programming/0-1-Knapsack.cpp
@@ -1,20 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 int max(int a, int b) { return (a > b) ? a : b; }
-pair<int, int> knapSack(int W, int wt[], int val[], int n){
+pair<int, int> knapSack(int W, int wt[], int pro[], int n){
     if (n == 0 || W == 0)
         return make_pair(0, 0);
     if (wt[n - 1] > W)
-        return knapSack(W, wt, val, n - 1);
-    pair<int, int> inc = knapSack(W - wt[n - 1], wt, val, n - 1);
-    inc.first += val[n - 1];
+        return knapSack(W, wt, pro, n - 1);
+    pair<int, int> inc = knapSack(W - wt[n - 1], wt, pro, n - 1);
+    inc.first += pro[n - 1];
     inc.second += wt[n - 1];
-    pair<int, int> exc = knapSack(W, wt, val, n - 1);
+    pair<int, int> exc = knapSack(W, wt, pro, n - 1);
     return (inc.first > exc.first) ? inc : exc;
 }
 int main(){
     int n;
-    cout << "Enter the number of items: ";cin >> n;
+    cout << "Enter the number of items: "; cin >> n;
     int profit[n],weight[n];
     cout << "Enter the profits for each item:\n";
     for (int i = 0; i < n; ++i) cin >> profit[i];
